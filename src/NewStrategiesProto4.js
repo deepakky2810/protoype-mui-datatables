08396896e8c5c1db348cfaddb55d76f2387f4c8c
Deepak Kumar Yadav <46112626+deepakky2810@users.noreply.github.com>
2022-03-09 09:40:13 +0530

    Proto4 form layout done

--- src/NewStrategiesProto4.js
+++ src/NewStrategiesProto4.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+  IconButton,
+  Typography,
   Checkbox,
   Button,
   Dialog,
@@ -11,11 +13,28 @@ import {
   Grid,
   InputLabel
 } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 import "./table.css";
 
 export const NewStrategiesProto4 = (props) => {
   const { handleClose, open } = props;
-  const [rows, setRows] = React.useState([]);
+  const [daysBeforeDepartureArr, setDaysBeforeDepartureArr] = React.useState([
+    ""
+  ]);
+  const [overbookingPercArr, setOverbookingPercArr] = React.useState([""]);
+  const [absoluteArr, setAbsoluteArr] = React.useState([""]);
+  const [forceArr, setForceArr] = React.useState([""]);
+
+  // React.useEffect(() => {
+  //   setDaysBeforeDepartureArr(strategyData?.days_before_departure);
+  //   setOverbookingPercArr(
+  //     strategyData?.data?.map((obj) => obj?.overbooking_percentage)
+  //   );
+  //   setAbsoluteArr(strategyData?.data?.map((obj) => obj?.absolute));
+  //   setForceArr(strategyData?.data?.map((obj) => obj?.force));
+  // }, [strategyData]);
+  // console.log(daysBeforeDepartureArr);
+
   const initialCacheState = {
     days_before_departure: "",
     overbookingPerc: "",
@@ -24,17 +43,23 @@ export const NewStrategiesProto4 = (props) => {
   };
   const [cache, setCache] = React.useState(initialCacheState);
   const handleSave = () => {
-    setRows([...rows, cache]);
-    setCache(initialCacheState);
+    setDaysBeforeDepartureArr([
+      ...daysBeforeDepartureArr,
+      initialCacheState.days_before_departure
+    ]);
+    setOverbookingPercArr([
+      ...overbookingPercArr,
+      initialCacheState.overbookingPerc
+    ]);
+    setAbsoluteArr([...absoluteArr, initialCacheState.absolute]);
+    setForceArr([...forceArr, initialCacheState.force]);
   };
-
   const isSaveDisabled = () => {
     const tempVal = new Set(
       Object.values(cache).filter((val) => val !== true && val !== false)
     );
     return tempVal.size === 1 && tempVal.has("");
   };
-
   const handleTextChange = (e, property) =>
     setCache({
       ...cache,
@@ -45,63 +70,114 @@ export const NewStrategiesProto4 = (props) => {
     <Dialog open={open} onClose={handleClose} maxWidth={"xs"} fullWidth>
       <DialogTitle id="new-strategy-dialogbox-title">New Strategy</DialogTitle>
       <DialogContent>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              id="dpInput"
-              label="DP"
-              type="text"
-              // variant="outlined"
-              value={cache.days_before_departure}
-              onChange={(e) => handleTextChange(e, "days_before_departure")}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id="overbooking%"
-              label="Overbooking %"
-              type="text"
-              // variant="outlined"
-              value={cache.overbooking_percentage}
-              onChange={(e) => handleTextChange(e, "overbooking_percentage")}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          {/* <Grid item xs={12}>
-            <Divider />
-          </Grid> */}
-          <Grid item xs={6}>
-            <TextField
-              id="absoluteValue"
-              label="Absolute Value"
-              type="text"
-              // variant="outlined"
-              value={cache.absolute}
-              onChange={(e) => handleTextChange(e, "absolute")}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          <Grid item xs={6} style={{ display: "flex", alignItems: "center" }}>
-            <Checkbox
-              size="small"
-              checked={cache.force}
-              onClick={(e) =>
-                setCache({
-                  ...cache,
-                  force: !!e.target.checked
-                })
-              }
-            />
-            <InputLabel>Force</InputLabel>
-          </Grid>
-        </Grid>
+        {daysBeforeDepartureArr?.map((dp, idx, arr) => (
+          <>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            >
+              <Grid item xs={3}>
+                <Typography
+                  style={{ fontWeight: "bold" }}
+                  variant="subtitle1"
+                >{`Rule ${idx + 1}`}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  id="dpInput"
+                  label="DP"
+                  type="text"
+                  // variant="outlined"
+                  value={daysBeforeDepartureArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...daysBeforeDepartureArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setDaysBeforeDepartureArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="overbooking%"
+                  label="Overbooking %"
+                  type="text"
+                  // variant="outlined"
+                  value={overbookingPercArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...overbookingPercArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setOverbookingPercArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="absoluteValue"
+                  label="Absolute Value"
+                  type="text"
+                  // variant="outlined"
+                  value={absoluteArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...absoluteArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setAbsoluteArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <Checkbox
+                  size="small"
+                  checked={forceArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...forceArr];
+                    temp.splice(idx, 1, e.target.checked);
+                    setForceArr([...temp]);
+                  }}
+                />
+                <InputLabel>Force</InputLabel>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            >
+              {idx === arr.length - 1 && (
+                <Grid item xs={2}>
+                  <IconButton
+                    onClick={handleSave}
+                    style={{
+                      padding: "0px",
+                      height: "20px",
+                      width: "20px",
+                      marginLeft: "10px"
+                    }}
+                  >
+                    <AddIcon style={{ fontSize: "1.7rem", fill: "#444" }} />
+                  </IconButton>
+                </Grid>
+              )}
+            </Grid>
+          </>
+        ))}
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
--- src/StrategiesProto4.js
+++ src/StrategiesProto4.js
@@ -24,6 +24,59 @@ export const StrategiesProto4 = (props) => {
   const { handleCancel, rowId, setRowId, onAddButtonClick, open } = props;
   const idx = overbooking.findIndex((row) => row.id === rowId);
   const strategyData = strats.find((obj) => obj.id === rowId);
+  const [daysBeforeDepartureArr, setDaysBeforeDepartureArr] = React.useState(
+    strategyData?.days_before_departure
+  );
+  const [overbookingPercArr, setOverbookingPercArr] = React.useState(
+    strategyData?.data?.map((obj) => obj?.overbooking_percentage)
+  );
+  const [absoluteArr, setAbsoluteArr] = React.useState(
+    strategyData?.data?.map((obj) => obj?.absolute)
+  );
+  const [forceArr, setForceArr] = React.useState(
+    strategyData?.data?.map((obj) => obj?.force)
+  );
+
+  React.useEffect(() => {
+    setDaysBeforeDepartureArr(strategyData?.days_before_departure);
+    setOverbookingPercArr(
+      strategyData?.data?.map((obj) => obj?.overbooking_percentage)
+    );
+    setAbsoluteArr(strategyData?.data?.map((obj) => obj?.absolute));
+    setForceArr(strategyData?.data?.map((obj) => obj?.force));
+  }, [strategyData]);
+  // console.log(daysBeforeDepartureArr);
+
+  const initialCacheState = {
+    days_before_departure: "",
+    overbookingPerc: "",
+    absolute: "",
+    force: false
+  };
+  const [cache, setCache] = React.useState(initialCacheState);
+  const handleSave = () => {
+    setDaysBeforeDepartureArr([
+      ...daysBeforeDepartureArr,
+      initialCacheState.days_before_departure
+    ]);
+    setOverbookingPercArr([
+      ...overbookingPercArr,
+      initialCacheState.overbookingPerc
+    ]);
+    setAbsoluteArr([...absoluteArr, initialCacheState.absolute]);
+    setForceArr([...forceArr, initialCacheState.force]);
+  };
+  const isSaveDisabled = () => {
+    const tempVal = new Set(
+      Object.values(cache).filter((val) => val !== true && val !== false)
+    );
+    return tempVal.size === 1 && tempVal.has("");
+  };
+  const handleTextChange = (e, property) =>
+    setCache({
+      ...cache,
+      [property]: e.target.value
+    });
 
   return (
     <Dialog open={open} onClose={handleCancel} maxWidth={"xs"} fullWidth>
@@ -71,65 +124,118 @@ export const StrategiesProto4 = (props) => {
         </div>
       </DialogTitle>
       <DialogContent>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              id="dpInput"
-              label="DP"
-              type="text"
-              // variant="outlined"
-              value={strategyData?.days_before_departure?.[0]}
-              onChange={() => {}}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id="overbooking%"
-              label="Overbooking %"
-              type="text"
-              // variant="outlined"
-              value={strategyData?.data?.overbooking_percentage}
-              onChange={() => {}}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          {/* <Grid item xs={12}>
-            <Divider />
-          </Grid> */}
-          <Grid item xs={6}>
-            <TextField
-              id="absoluteValue"
-              label="Absolute Value"
-              type="text"
-              // variant="outlined"
-              value={strategyData?.data?.absolute}
-              onChange={() => {}}
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          <Grid item xs={6} style={{ display: "flex", alignItems: "center" }}>
-            <Checkbox
-              size="small"
-              checked={strategyData?.data?.force}
-              onClick={() => {}}
-            />
-            <InputLabel>Force</InputLabel>
-          </Grid>
-        </Grid>
+        {daysBeforeDepartureArr?.map((dp, idx, arr) => (
+          <>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            >
+              <Grid item xs={12}>
+                <Typography
+                  style={{ fontWeight: "bold" }}
+                  variant="subtitle1"
+                >{`Rule ${idx + 1}`}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  id="dpInput"
+                  label="DP"
+                  type="text"
+                  // variant="outlined"
+                  value={daysBeforeDepartureArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...daysBeforeDepartureArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setDaysBeforeDepartureArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="overbooking%"
+                  label="Overbooking %"
+                  type="text"
+                  // variant="outlined"
+                  value={overbookingPercArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...overbookingPercArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setOverbookingPercArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="absoluteValue"
+                  label="Absolute Value"
+                  type="text"
+                  // variant="outlined"
+                  value={absoluteArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...absoluteArr];
+                    temp.splice(idx, 1, e.target.value);
+                    setAbsoluteArr([...temp]);
+                  }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <Checkbox
+                  size="small"
+                  checked={forceArr?.[idx]}
+                  onChange={(e) => {
+                    let temp = [...forceArr];
+                    temp.splice(idx, 1, e.target.checked);
+                    setForceArr([...temp]);
+                  }}
+                />
+                <InputLabel>Force</InputLabel>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            >
+              {idx === arr.length - 1 && (
+                <Grid item xs={2}>
+                  <IconButton
+                    onClick={handleSave}
+                    style={{
+                      padding: "0px",
+                      height: "20px",
+                      width: "20px",
+                      marginLeft: "10px"
+                    }}
+                  >
+                    <AddIcon style={{ fontSize: "1.7rem", fill: "#444" }} />
+                  </IconButton>
+                </Grid>
+              )}
+            </Grid>
+          </>
+        ))}
       </DialogContent>
       <DialogActions>
         <Button onClick={handleCancel} color="primary">
           Cancel
-        </Button>
-        <Button onClick={() => {}} color="primary">
-          Save Changes
         </Button>
       </DialogActions>
     </Dialog>
